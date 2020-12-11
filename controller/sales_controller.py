647a53a2d006706e78c66acf385d61d5f032c578
KriszTuri <turi.krisztina1999@gmail.com>
2020-12-11 09:42:08 +0100

    sales modules done

--- controller/sales_controller.py
+++ controller/sales_controller.py
@@ -1,81 +1,123 @@
 from model.sales import sales
-from view import terminal as view
+from view import terminal_view
+from model import data_manager
 
 
-def list_transactions():
-    view.print_error_message("Not implemented yet.")
+def run():
+
+    options = ["Add a new transaction",
+               "Filter transactions by the employee",
+               "Filter transactions by the customer",
+               "Get the sales of games by a manufacturer",
+               "Get a ranking of sold items per manufacturer",
+               "Get a ranking of sold items and earned money",
+               "Get all the granted discounts",
+               "Get the most earning employee",
+               "Get the worst earning employee",
+               "Get all employee sales results"]
+
+    welcome = """
+              #####     #        #####     #
+             #     #   # #      #     #   # #
+             #        #   #     #        #   #
+              #####  #     #     #####  #     #
+                   # #######          # #######
+             #     # #     #    #     # #     #
+              #####  #     #     #####  #     #
+
+             #       #######    #       #######
+             #       #          #       #
+             #       #          #       #
+             #       #####      #       #####
+             #       #          #       #
+             #       #          #       #
+             ####### #######    ####### #######
 
 
-def add_transaction():
-    view.print_error_message("Not implemented yet.")
+    """
 
+    choice = None
 
-def update_transaction():
-    view.print_error_message("Not implemented yet.")
+    while choice != "0":
+        choice = terminal_view.get_choice_store(welcome, options)
+        table = sales.get_table()
 
+        if choice == "1":
+            label = "Provide new record /n"
+            id = sales.generate_random(table)
+            id_position = 0 
+            title_position = 1
 
-def delete_transaction():
-    view.print_error_message("Not implemented yet.")
+            store_table = data_manager.get_table_from_file("model/store/games.csv")
+            store_id_list = data_manager.get_column_from_table(store_table, id_position)
+            store_titles_list = data_manager.get_column_from_table(store_table, title_position)
+            store_id = terminal_view.get_input_from_list(store_id_list, store_titles_list, "Provide correct store ID, or game title ")
 
+            hr_table = data_manager.get_table_from_file("model/hr/persons.csv")
+            hr_id_list = data_manager.get_column_from_table(hr_table, id_position)
+            hr_titles_list = data_manager.get_column_from_table(hr_table, title_position)
+            hr_id = terminal_view.get_input_from_list(hr_id_list, hr_titles_list, "Provide correct employee ID, or name")
 
-def get_biggest_revenue_transaction():
-    view.print_error_message("Not implemented yet.")
+            crm_table = data_manager.get_table_from_file("model/crm/customers.csv")
+            crm_id_list = data_manager.get_column_from_table(crm_table, id_position)
+            crm_titles_list = data_manager.get_column_from_table(crm_table, title_position)
+            crm_id = terminal_view.get_input_from_list(crm_id_list, crm_titles_list, "Provide correct customer ID, or name")
 
+            qty = terminal_view.get_inputs(["Quantity: "], "Provide item quantity")
+            table_updated = sales.add_transaction(table, id, store_id, hr_id, crm_id, qty[0])
+            data_manager.write_table_to_file("model/sales/sales.csv", table_updated)
 
-def get_biggest_revenue_product():
-    view.print_error_message("Not implemented yet.")
+        elif choice == "2":
+            label = "The transactions made by given employee: "
+            employee_id = terminal_view.get_inputs(["Employee ID: "], "Provide the employee ID to search for his/her transactions")
+            result = sales.filter_by_employee(table, employee_id[0])
+            terminal_view.print_result(result, label)
 
+        elif choice == "3":
+            label = "The transactions made by given customer: "
+            customer_id = terminal_view.get_inputs(["Customer ID: "], "Provide customer's ID to search for his/her transactions")
+            result = sales.filter_by_customer(table, customer_id[0])
+            terminal_view.print_result(result, label)
 
-def count_transactions_between():
-    view.print_error_message("Not implemented yet.")
+        elif choice == "4":
+            label = "Number of games sold by a given manufacturer: "
+            manufacturer = terminal_view.get_inputs(["Manufacturer: "], "Provide manufacturer's name")
+            result = sales.filter_by_manufacturer(table, manufacturer[0])
+            terminal_view.print_result(result, label)
 
+        elif choice == "5":
+            label = "Manufacturer | sold copies"
+            result = sales.rank_by_manufacturer(table)
+            terminal_view.print_result(result, label)
 
-def sum_transactions_between():
-    view.print_error_message("Not implemented yet.")
+        elif choice == "6":
+            raport = sales.generate_raport(table)
+            raport_header = ["title", "total earnings"]
+            terminal_view.print_table(raport, raport_header)
+            data_manager.write_table_to_file("model/sales/raport.csv", raport)
 
+        elif choice == "7":
+            label = "All discounts granted"
+            result = sales.get_discounts(table)
+            terminal_view.print_result(result, label)
+        
+        elif choice == "8":
+            label = "Most earning employee"
+            result = sales.most_earned(table)
+            terminal_view.print_result(result, label)
 
-def run_operation(option):
-    if option == 1:
-        list_transactions()
-    elif option == 2:
-        add_transaction()
-    elif option == 3:
-        update_transaction()
-    elif option == 4:
-        delete_transaction()
-    elif option == 5:
-        get_biggest_revenue_transaction()
-    elif option == 6:
-        get_biggest_revenue_product()
-    elif option == 7:
-        count_transactions_between()
-    elif option == 8:
-        sum_transactions_between()
-    elif option == 0:
-        return
-    else:
-        raise KeyError("There is no such option.")
+        elif choice == "9":
+            label = "Min earning employee"
+            result = sales.min_earned(table)
+            terminal_view.print_result(result, label)
 
+        elif choice == "10":
+            label = "All employee sales results"
+            result = sales.employees_earning(table)
+            terminal_view.print_result(result, label)
 
-def display_menu():
-    options = ["Back to main menu",
-               "List transactions",
-               "Add new transaction",
-               "Update transaction",
-               "Remove transaction",
-               "Get the transaction that made the biggest revenue",
-               "Get the product that made the biggest revenue altogether",
-               "Count number of transactions between",
-               "Sum the price of transactions between"]
-    view.print_menu("Sales", options)
+        elif choice == "0":
+            terminal_view.print_result("", "You are going back to the main menu")
 
-
-def menu():
-    operation = None
-    while operation != '0':
-        display_menu()
-        try:
-            operation = view.get_input("Select an operation")
-            run_operation(int(operation))
-        except KeyError as err:
-            view.print_error_message(err)
+        else:
+            terminal_view.print_error_message("There is no such choice.")
--- model/sales/sales.py
+++ model/sales/sales.py
@@ -1,14 +1,281 @@
-""" Sales module
+""" Sales module """
 
-Data table structure:
-    - id (string)
-    - customer id (string)
-    - product (string)
-    - price (float)
-    - transaction date (string): in ISO 8601 format (like 1989-03-21)
-"""
+# modules import go here:
+import random
 
-from model import data_manager, util
+from model import data_manager
+from model.store import store
+from model.hr import hr
+from model.crm import crm
 
-DATAFILE = "model/sales/sales.csv"
-HEADERS = ["Id", "Customer", "Product", "Price", "Date"]
+# global variables go here:
+
+CUSTOMER_ID = 2
+PRODUCT_ID = 3
+NUMBER_OF_ITEMS = 4
+
+STORE_PRODUCT_ID = 0
+STORE_MANUFACTURER = 2
+
+
+# sales.csv: 'id, employee_id, customer_id, product_id, number_of_items'
+
+
+def get_table():
+    file = "model/sales/sales.csv"
+    table = data_manager.get_table_from_file(file)
+    return table
+
+
+def generate_random(table):
+
+    id_list = []
+    generated = ""
+    for game in table:
+        id = game[0]
+        id_list.append(id)
+
+    expected_length = 8
+    while generated in id_list or len(generated) != expected_length:
+        quantity = 2
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        characters = '!@#$%^&*()_+=-{}[]|\:"<>,.?/'
+        random_letters = [random.choice(alphabet) for i in range(quantity)] + [random.choice(alphabet).upper() for i in range(quantity)]
+        random_characters = [random.choice(characters) for i in range(quantity)]
+        random_numbers = [str(random.randint(0, 9)) for i in range(quantity)]
+        generated_list = [letter for letter in random_letters] + [character for character in random_characters] + [digit for digit in random_numbers]
+        random.shuffle(generated_list)
+        generated = "".join(generated_list)
+
+    return generated
+
+
+def add_transaction(table, id, store_id, hr_id, crm_id, quantity):
+
+    record = [id, store_id, hr_id, crm_id, quantity]
+    table.append(record)
+
+    return table
+
+
+def filter_by_employee(table, employee_id):
+
+    operations = []
+    employee_id_index = 1
+    for record in table:
+        id = record[employee_id_index]
+        if id == employee_id:
+            operations.append(record)
+    return operations
+
+
+def filter_by_customer(table, customer):
+
+    transactions = []
+    for record in table:
+        if record[CUSTOMER_ID] == customer:
+            transactions.append(record)
+    return transactions
+
+
+def filter_by_manufacturer(table, manufacturer):
+
+
+    store_table = store.get_table()
+
+    store_id_list = []
+    for record in store_table:
+        if record[STORE_MANUFACTURER] == manufacturer:
+            store_id_list.append(record[STORE_PRODUCT_ID])
+
+    sales = 0
+    for record in table:
+        if record[PRODUCT_ID] in store_id_list:
+            sales += int(record[NUMBER_OF_ITEMS])
+
+    return sales
+
+
+def employees_earning(table):
+   
+
+    product_index = 1
+    employee_id_index = 2
+    amount_sold_index = 4
+
+    person_id_index = 0
+    person_name_index = 1
+
+    game_index = 0
+    price_index = 3
+
+    store_table = store.get_table()
+    store.check_table(store_table)
+    hr_table = hr.get_table('model/hr/persons.csv')
+    money_earned = {}
+    for person in hr_table:
+        person_id = person[person_id_index]
+        person_name = person[person_name_index]
+        money_earned[person_name] = 0
+        for record in table:
+            product_id = record[product_index]
+            employee_id = record[employee_id_index]
+            amount_sold = int(record[amount_sold_index])
+            if person_id == employee_id:
+                for game in store_table:
+                    game_id = game[game_index]
+                    if game_id == product_id:
+                        game_price = int(game[price_index])
+                        money_earned[person_name] += int(amount_sold * game_price)
+    return money_earned
+
+
+def most_earned(table):
+
+    money_earned = employees_earning(table)
+    temp = 0
+    for employee in money_earned:
+        if money_earned[employee] > temp:
+            temp = money_earned[employee]
+            most_earned_employee = str(employee) + ":" + str(money_earned[employee])
+    return most_earned_employee
+
+
+def min_earned(table):
+
+    money_earned = employees_earning(table)
+    temp = 0
+    min_earned_employee = [0]
+    for employee in money_earned:
+        if temp == 0:
+            temp = money_earned[employee]
+        if money_earned[employee] <= temp:
+            temp = money_earned[employee]
+            if money_earned[employee] > 0:
+                min_earned_employee[0] = (str(employee) + ": " + str(money_earned[employee]))
+            elif money_earned[employee] == 0:
+                min_earned_employee.append(str(employee) + ": " + str(money_earned[employee]))
+    if len(min_earned_employee) > 1:
+        min_earned_employee.pop(0)
+    return min_earned_employee
+
+
+def rank_by_manufacturer(table):
+
+    manufacturers_counts = {}
+    product_id_index = 3
+    amount_sold_index = 4
+    id_index = 0
+    manufacturer_index = 2
+
+    store_table = store.get_table()
+    store.check_table(store_table)
+
+    for record in table:
+        product_id = record[product_id_index]
+        amount_sold = record[amount_sold_index]
+        for game in store_table:
+            game_id = game[id_index]
+            manufacturer = game[manufacturer_index]
+            if product_id == game_id:
+                if manufacturer in manufacturers_counts:
+                    manufacturers_counts[manufacturer] += int(amount_sold)
+                else:
+                    manufacturers_counts[manufacturer] = int(amount_sold)
+
+    return manufacturers_counts
+
+
+def get_sold_copies(table, store_id):
+
+
+    store_id_position = 1
+    quantity_position = 4
+    sold_copies = 0
+
+    for record in table:
+        if str(record[store_id_position]) == str(store_id):
+            sold_copies = sold_copies + int(record[quantity_position])
+
+    return sold_copies
+
+
+def generate_raport(table):
+
+    item_table = store.get_table()
+    item_table = store.check_table(item_table)
+    raport = []
+
+    id_position = 0
+    title_position = 1
+    price_position = 3
+    earnings = 0
+
+    for item in item_table:
+        price = item[price_position]
+        sold_copies = get_sold_copies(table, item[id_position])
+        if price and sold_copies:
+            earnings = int(price) * int(sold_copies)
+            raport_row = [item[title_position], str(earnings)]
+            raport.append(raport_row)
+
+    return raport
+
+
+def get_discounts(table):
+
+
+    discounts_money_spent = {}
+    discount_levels = [100, 1000, 5000]
+    discount_percents = ["2%", "5%", "10%"]
+
+    customer_id_index = 2
+    amount_sold_index = 4
+    id_index = 0
+    product_id_index = 3
+    name_index = 1
+    price_index = 3
+
+    store_table = store.get_table()
+    store.check_table(store_table)
+
+    crm_table = crm.get_table('model/crm/customers.csv')
+
+    for record in table:
+        product_id = record[product_id_index]
+        amount_sold = record[amount_sold_index]
+        customer_id = record[customer_id_index]
+
+        for customer in crm_table:
+            customer_name = customer[name_index]
+            client_id = customer[id_index]
+
+            if customer_id == client_id:
+
+                for game in store_table:
+                    game_id = game[id_index]
+                    price = game[price_index]
+                    if product_id == game_id:
+                        money_spent = int(price) * int(amount_sold)
+
+                        if customer_name in discounts_money_spent:
+                            discounts_money_spent[customer_name] += int(money_spent)
+                        else:
+                            discounts_money_spent[customer_name] = int(money_spent)
+
+    discounts = {}
+
+    for customer_name in discounts_money_spent:
+        money_spent = discounts_money_spent[customer_name]
+        if money_spent >= discount_levels[0] and money_spent < discount_levels[1]:
+            discount = discount_percents[0]
+        elif money_spent >= discount_levels[1] and money_spent < discount_levels[2]:
+            discount = discount_percents[1]
+        elif money_spent >= discount_levels[2]:
+            discount = discount_percents[2]
+        else:
+            discount = "0%"
+
+        discounts[customer_name] = discount
+
+    return discounts
