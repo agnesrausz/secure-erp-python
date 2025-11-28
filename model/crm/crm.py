dc538723e5f34e6549e83b67dadedf2c8e137f96
agnesrausz <rauszka94@gmail.com>
2025-11-28 20:37:51 +0100

    implement add_customer

--- model/crm/crm.py
+++ model/crm/crm.py
@@ -12,3 +12,17 @@ from model import data_manager, util
 
 DATAFILE = "model/crm/crm.csv"
 HEADERS = ["id", "name", "email", "subscribed"]
+
+
+def add_customer(customer):
+    table = data_manager.read_table_from_file(DATAFILE)
+    customer["id"] = util.generate_id()
+    if (not customer.get("id") or
+            not customer.get("name") or
+            not customer.get("email") or
+            not customer.get("subscribed")):
+        raise KeyError("Customer dictionary is missing required fields.")
+    table_row = [customer["id"], customer["name"], customer["email"], customer["subscribed"]]
+    table.append(table_row)
+    data_manager.write_table_to_file(DATAFILE, table)
+    return customer["id"]
